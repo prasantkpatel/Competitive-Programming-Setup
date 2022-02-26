381779bc5d8d95a56a6bd22e1577cf48f95958e1
Prasant Patel <prasant@Mania.local>
2022-02-27 03:09:02 +0530

    Updated grid snippet

--- a.cpp
+++ a.cpp
@@ -28,35 +28,6 @@ using namespace std;
 #define all(v) v.begin(), v.end()
 #define case_g(x) cout<<"Case #"<<x<<": "
 
-// Debugging template
-#ifndef LOCAL
-#define cerr if (false) cerr
-#endif
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-template<typename T>
-void _printA(T *t, long long sz) { cout<<" { "; for (long long i=0; i<sz; i++) cout<<"["<<i<<"] = "<< t[i]<<endl; cout<<" } \n";}
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
-#define debugA(x, y) cerr << "[" << #x << "] = "; _printA(x, y)
-
 // Aliases
 using ll = long long;
 using ld = long double;
@@ -84,169 +55,179 @@ constexpr char nl = '\n';
 void precompute() {
 }
 
-// Sparse Table
-template<typename T>
-class sparse_table {
+// 2D Grid
+template <typename T>
+class grid {
 private:
-	inline static int LOG(int n) {
-		return sizeof(int) * CHAR_BIT - __builtin_clz(n) - 1;
-	}
-
-	struct op {
+	struct node {
 	private:
-		T __identity;
-		function<T(T, T)> __combinator;
-		bool __is_overlap_agnostic; // true if queries can be answered by overlapping ranges
+		T val;
+		int i, j;
+		vector<pair<int, int>> neighbours;
 
 	public:
-		op() {}
-		op(T _identity, function<T(T, T)> _combinator, bool _is_overlap_agnostic) {
-			__identity = _identity;
-			__combinator = _combinator;
-			__is_overlap_agnostic = _is_overlap_agnostic;
+		node() {
+			val = T();
+			i = -1, j = -1;
 		}
 
-		T inline combine(const T &x, const T &y) {
-			return __combinator(x, y);
+		void set(int _i, int _j) {
+			i = _i;
+			j = _j;
 		}
 
-		T inline identity() {
-			return __identity;
+		vector<pair<int, int>>& next() {
+			return neighbours;
 		}
 
-		bool inline is_overlap_agnostic() {
-			return __is_overlap_agnostic;
+		bool operator==(T x) {return val == x;}
+		bool operator!=(T x) {return val != x;}
+		T operator=(T x) {return (val = x);}
+		T operator+(T x) {return val + x;}
+		T operator-(T x) {return val - x;}
+		T operator*(T x) {return val * x;}
+		T operator/(T x) {return val / x;}
+		T operator&&(T x) {return val && x;}
+		T operator||(T x) {return val || x;}
+		T operator^(T x) {return val ^ x;}
+		T operator&(T x) {return val & x;}
+		T operator|(T x) {return val | x;}
+
+		friend ostream& operator<<(ostream &o, node &obj) {
+			o << obj.val;
+			return o;
+		}
+
+		friend istream& operator>>(istream &i, node &obj) {
+			i >> obj.val;
+			return i;
+		}
+
+		/* Gives the next node from (i, j) according to the direction.
+		 * Valid directions are u and U for moving up. Rest three
+         * directions are similar.
+		*/
+		pair<int, int> operator[](char dir) {
+			pair<int, int> res;
+			if(dir == 'U' || dir == 'u') {
+				res = {i - 1, j};
+
+			} else if(dir == 'D' || dir == 'd') {
+				res = {i + 1, j};
+
+			} else if(dir == 'L' || dir == 'l') {
+				res = {i, j - 1};
+
+			} else if(dir == 'R' || dir == 'r') {
+				res = {i, j + 1};
+
+			} else {
+				cerr << "Error: invalid direction" << nl;
+				res = {-1, -1};
+			}
+			return res;
+		}
+
+		/* Gives the next node from (i, j) according to the direction.
+		 * Valid directions are up, UP, uP, Up, u, and U for moving up. 
+		 * Rest three directions are similar.
+		*/
+		pair<int, int> operator[](string dir) {
+			for(auto &ch : dir)
+				ch = tolower(ch);
+
+			pair<int, int> res;
+			if(dir == "up" || dir == "u") {
+				res = {i - 1, j};
+
+			} else if(dir == "down" || dir == "d") {
+				res = {i + 1, j};
+
+			} else if(dir == "left" || dir == "l") {
+				res = {i, j - 1};
+
+			} else if(dir == "right" || dir == "r") {
+				res = {i, j + 1};
+
+			} else {
+				cerr << "Error: invalid direction" << nl;
+				res = {-1, -1};
+			}
+			return res;
 		}
 	};
 
-	vector<vector<T>> table;
-	int n, spS;
-	op operation;
+	int n, m;
+	vector<vector<node>> mat;
+
+	static constexpr int dx[4] = {1, -1, 0, 0};
+	static constexpr int dy[4] = {0, 0, 1, -1};
 
 public:
-	struct use {
-	public:
-		const static inline op MIN = op(numeric_limits<T>::max(), [](const T &x, const T &y) {return min(x, y);}, true);
-		const static inline op MAX = op(numeric_limits<T>::min(), [](const T &x, const T &y) {return max(x, y);}, true);
-		const static inline op GCD = op(T(0), [](const T &x, const T &y) {return __gcd(x, y);}, true);
-		const static inline op SUM = op(T(0), [](const T &x, const T &y) {return x + y;}, false);
-		const static inline op XOR = op(T(0), [](const T &x, const T &y) {return x ^ y;}, false);
-		const static inline op AND = op(T(1), [](const T &x, const T &y) {return x & y;}, true);
-		const static inline op OR = op(T(0), [](const T &x, const T &y) {return x | y;}, true);
-	};
+	grid(int _n, int _m) {
+		n = _n, m = _m;
+		mat.resize(n);
+		for(auto &v : mat) {
+			v.resize(m, node());
+		}
 
-	sparse_table(const op &_operation) {
-		n = 0, spS = 0;
-		operation = _operation;
-	}
-
-	sparse_table(T _identity, function<T(T, T)> _combinator, bool _is_overalp_agnostic) {
-		n = 0, spS = 0;
-		operation = op(_identity, _combinator, _is_overalp_agnostic);
-	}
-
-	void build(int _n) {
-		n = _n;
-		spS = LOG(n);
-
-		table.resize(n);
 		for(int i = 0; i < n; ++i) {
-			table[i].resize(spS + 1, operation.identity());
-		}
-	}
-
-	template <typename V>
-	void build(vector<V> &a) {
-		n = max(n, (int)a.size());
-		spS = LOG(n);
-
-		table.resize(n);
-		for(int i = 0; i < n; ++i) {
-			table[i].resize(spS + 1, operation.identity());
-		}
-
-		for(int i = 0; i < (int)a.size(); ++i) {
-			table[i][0] = static_cast<T>(a[i]);
-		}
-
-		for(int j = 1; j <= spS; ++j) {
-			for(int i = 0; i + (1 << j) <= (int)a.size(); ++i) {
-				table[i][j] = operation.combine(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
-			}
-		}
-	}
-
-	template <typename V>
-	void build(V a[], int m) {
-		n = max(n, m);
-		spS = LOG(n);
-
-		table.resize(n);
-		for(int i = 0; i < n; ++i) {
-			table[i].resize(spS + 1, operation.identity());
-		}
-
-		for(int i = 0; i < m; ++i) {
-			table[i][0] = static_cast<T>(a[i]);
-		}
-
-		for(int j = 1; j <= spS; ++j) {
-			for(int i = 0; i + (1 << j) <= m; ++i) {
-				table[i][j] = operation.combine(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
-			}
-		}
-	}
-
-	T query(int l, int r) {
-		T res = operation.identity();
-		if(l < 0 || r < 0 || l >= n || r >= n || l > r) {
-			cerr << "Error: invalid range [" << l << ", " << r << "] in query to sparse table" << nl;
-
-		} else if(operation.is_overlap_agnostic()) {
-			res = operation.combine(table[l][LOG(r - l + 1)], table[r - (1 << LOG(r - l + 1)) + 1][LOG(r - l + 1)]);
-			
-		} else {
-			while(l <= r) {
-				res = operation.combine(res, table[l][LOG(r - l + 1)]);
-				l += 1 << LOG(r - l + 1);
-			}
-		}
-		return res;
-	}
-};
-sparse_table<int> sps(sparse_table<int>::use::GCD);
-
-void solve(int tc=1) {
-	int n;
-	cin >> n;
-	
-	int a[2*n];
-	for(int i = 0; i < n; ++i) cin >> a[i], a[i + n] = a[i];
-	
-	sps.build(a, 2 * n);
-
-	int x = 0;
-	for(int j = n; j > 0; j /= 2) {
-		
-		while(x + j <= n) {
-			int k = x + j, g = sps.query(0, k - 1);
-			bool ok = 0;
-			
-			for(int i = 1; i < n; ++i) {
-				if(sps.query(i, i + k - 1) != g) {
-					x += j, ok = 1;
-					break;
+			for(int j = 0; j < m; ++j) {
+				for(int k = 0; k < 4; ++k) {
+					if(is_valid(i + dx[k], j + dy[k])) {
+						mat[i][j].next().push_back({i + dx[k], j + dy[k]});
+						mat[i][j].set(i, j);
+					}
 				}
 			}
-			if(!ok) break;
 		}
 	}
-	cout << x << nl;
+
+	vector<node>& operator[](int idx) {
+		return mat[idx];
+	}
+
+	bool is_valid(int i, int j) {
+		return (i >= 0 && j >= 0 && i < n && j < m);
+	}
+};
+
+bool vis[1000][1000];
+void dfs(grid<char> &a, int i, int j) {
+	if(vis[i][j])
+		return;
+
+	vis[i][j] = 1;
+	for(auto [ix, jx] : a[i][j].next()) {
+		if(a[ix][jx] == '.')
+			dfs(a, ix, jx);
+	}
+}
+
+void solve(int tc=1) {
+	int n, m;
+	cin >> n >> m;
+
+	grid<char> a(n, m);
+	for(int i = 0; i < n; ++i)
+		for(int j = 0; j < m; ++j)
+			cin >> a[i][j];
+
+	int ans = 0;
+	for(int i = 0; i < n; ++i) {
+		for(int j = 0; j < m; ++j) {
+			if(a[i][j] == '.' && !vis[i][j]) {
+				++ans;
+				dfs(a, i, j);
+			}
+		}
+	}
+	cout << ans << nl;
 }
 
 int main() {
 	nitro;
-	
+
 	#ifdef USE_IO_FILES
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
@@ -257,7 +238,6 @@ int main() {
 
 	int tc = 1;
 
- 	cin >> tc;
 	for(int i = 1; i <= tc; ++i) {
 		//case_g(i);
 		solve(i);
